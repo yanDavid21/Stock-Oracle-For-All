056c811a0bd61f2c6a0f1b8a403fe879fa390bb0
Khoa Vo <vo.kh@northeastern.edu>
2023-04-11 11:07:34 -0400

    Added def for fetching price

--- mercury/adapters/yahoo_finance_api/ops/fetch_latest_price.py
+++ mercury/adapters/yahoo_finance_api/ops/fetch_latest_price.py
@@ -1,7 +1,8 @@
 from typing import Any, Dict, List, Optional, Set, Union
 
-from dagster import OpDefinition, get_dagster_logger, op
-
+from dagster import OpDefinition, OpExecutionContext, get_dagster_logger, op
+import requests
+import redis
 from mercury._utils import CategoryKeyError, build_id
 from mercury.adapters.yahoo_finance_api import YahooFinanceApiCategory
 from mercury.base.base_op import BaseCategorizedOp, BaseCategorizedOpFactory
@@ -18,17 +19,34 @@ class YahooFinanceApiFetchLatestPriceOp(BaseCategorizedOp):
     ) -> None:
         super().__init__(category, provider, required_resource_keys, config_schema)
 
+    def _get_price(self, key: str, ticker: str) -> str:
+        """Crawl price data from API operation
+        
+        Args:
+            page_url (str): the url link of the api
+            headers (str): the key for your api
+            ticker (str): the stock ticker
+        Returns: the current price of the stock ticker"""
+        get_dagster_logger().info('Begin crawling...')
+        page_url = f"https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/{ticker}/financial-data"
+        headers = {"X-RapidAPI-Key": key,
+            "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"}
+        response = requests.request("GET", page_url, headers=headers)
+        price = response.json()['financialData']['currentPrice']['raw']
+        get_dagster_logger().info('Finish crawling!')
+        return f"{ticker}:{price}"
+
     def build(self, **kwargs) -> OpDefinition:
         @op(
             name=build_id(provider=self.provider, identifier=f"fetch_{self.category}_stock_db_op"),
             config_schema=self.config_schema,
             required_resource_keys=self.required_resource_keys,
             **kwargs,
-        )
-        def _op():
-            # Main log to fetch data from data srouce goes here
-            data = ""
-            get_dagster_logger().log(data)
+        ) 
+        def _op(context) -> str:
+
+            # Main log to fetch data from data source goes here
+            return self._get_price("70f59a384fmsh1cfc6e9694781c3p1107f5jsna9f6206b0c3d", "NFLX")
 
         return _op
 
