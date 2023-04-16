9b544be1e134d5a42305bfa1d312dbf19e54f558
Khoa Vo <vo.kh@northeastern.edu>
2023-04-16 14:39:23 -0400

    Add stock list

---
+++ mercury/_utils/stock_list.py
@@ -0,0 +1,8 @@
+import json
+
+
+class StockList:
+    def __init__(self):
+        self.data = ["AAPL", "MSFT", "GOOG", "AMZN", "BRK.A", "NVDA", "TSLA", "META", "V", "UNH", "XOM", "JNJ", "JPM", 
+                     "WMT", "LLY", "PG", "MA", "CVX", "HD", "MRK", "ABBV", "KO", "ORCL",
+                       "AVGO", "PEP", "BAC", "PFE", "TMO", "COST", "MCD"]
